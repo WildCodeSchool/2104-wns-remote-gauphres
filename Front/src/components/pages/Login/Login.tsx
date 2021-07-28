1e9e9b076fe281618fea7d406e8aa0355e55dd12
Alexis <alexis@izicamp.fr>
2021-07-28 12:35:46 +0200

    Add loginPage style

--- Front/src/components/pages/Login/Login.tsx
+++ Front/src/components/pages/Login/Login.tsx
@@ -1,10 +1,19 @@
 import React, { FC } from 'react';
+import { Title, Container, Form, TextInput, Input } from './style';
+import Button from '../../Button/Button';
 
 const LoginPage: FC = () => {
     return (
-        <> 
-            <div>Login Page</div>
-        </>
+        <Container>
+            <Form>
+                <Title>Connecte toi !</Title>
+                <Input>
+                    <TextInput label="Email" variant="outlined" />
+                    <TextInput label="Mot de passe" variant="outlined" />
+                </Input>
+                <Button>Se connecter</Button>
+            </Form>
+        </Container>
     );
 };
 
