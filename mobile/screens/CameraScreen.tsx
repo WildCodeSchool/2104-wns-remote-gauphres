338844fea26c9b2c7c13c180e47b6eab2867f63d
Benoit Deligny <benoit.deligny@manomano.com>
2021-09-05 19:34:56 +0200

    adding simple code to take picture back&front camera

--- mobile/screens/CameraScreen.tsx
+++ mobile/screens/CameraScreen.tsx
@@ -1,11 +1,73 @@
-import React from "react";
-import { Text, View } from "react-native";
+import React, { useState, useEffect, useRef } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, Button } from 'react-native';
+import { Camera } from 'expo-camera';
 
-const CameraScreen = () => {
-    return (
-        <View>
-            <Text>Hello, world! </Text>
+export default function App() {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [type, setType] = useState(Camera.Constants.Type.back);
+  const cameraRef = useRef(null);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestPermissionsAsync();
+      setHasPermission(status === 'granted');
+    })();
+  }, []);
+
+  if (hasPermission === null) {
+    return <View />;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+  return (
+    <View style={styles.container}>
+      <Camera style={styles.camera} type={type} ref={cameraRef}>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => {
+              setType(
+                type === Camera.Constants.Type.back
+                  ? Camera.Constants.Type.front
+                  : Camera.Constants.Type.back
+              );
+            }}>
+            <Text style={styles.text}> Flip </Text>
+          </TouchableOpacity>
         </View>
+      </Camera>
+      <Button
+        title="Take a picture"
+        onPress={async () => {
+          const pictureMetadata = await cameraRef.current.takePictureAsync();
+          console.log("pictureMetadata", pictureMetadata);
+        }}
+      />
+    </View>
   );
-};
-export default CameraScreen;
+}
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+    },
+    camera: {
+      flex: 1,
+    },
+    buttonContainer: {
+      flex: 1,
+      backgroundColor: 'transparent',
+      flexDirection: 'row',
+      margin: 20,
+    },
+    button: {
+      flex: 0.1,
+      alignSelf: 'flex-end',
+      alignItems: 'center',
+    },
+    text: {
+      fontSize: 18,
+      color: 'white',
+    },
+  });
