82f66b23bdeea1c48c321656f4f5fba898814f5d
Aline Denizot <aline.denizot2@gmail.com>
2021-10-06 14:14:36 +0200

    test2 webhook

--- Front/src/components/pages/HomePage/HomePage.tsx
+++ Front/src/components/pages/HomePage/HomePage.tsx
@@ -5,6 +5,7 @@ import Button from '../../Button/Button';
 const HomePage: FC = () => {
     return (
         <>
+            <div>Home Page</div>
             <Button>
                 <Link to="/login">Se connecter</Link>
             </Button>
