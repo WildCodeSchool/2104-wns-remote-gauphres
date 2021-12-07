eccf168e8af368ff4ad1fd1783b910893fb12c64
Aline Denizot <aline.denizot2@gmail.com>
2021-12-07 16:03:29 +0100

    create memberpage

--- Front/src/components/pages/Members/Members.tsx
+++ Front/src/components/pages/Members/Members.tsx
@@ -1,13 +1,58 @@
 import { Link } from 'react-router-dom';
-import React, { FC } from 'react';
+import React, { FC, useContext } from 'react';
+import { gql, useQuery } from '@apollo/client';
 import SideMenu from '../../SideMenu/SideMenu';
 import { SideMenuContainer } from '../../../style';
+import { User, UserContext } from '../../../contexts/UserContext';
+import { MemberCard, UserMember } from '../../Chat/MemberCard/MemberCard';
+import { MemberPage, MembersContainer, MemberTitle } from './style';
+
+const FIND_ALL_USERS = gql`
+    query getAllUsers {
+        getAllUsers {
+            _id
+            username
+            firstname
+            lastname
+            avatar
+            isConnected
+            birthDate
+            userMood {
+                title
+                image
+            }
+            hobbies
+        }
+    }
+`;
 
 const MembersPage: FC = () => {
+    const { user } = useContext(UserContext);
+
+    const { loading, data: getAllUsers } = useQuery(FIND_ALL_USERS);
+
+    let otherUsers;
+
+    if (!loading && getAllUsers) {
+        if (getAllUsers.getAllUsers.length > 0 && user) {
+            otherUsers = getAllUsers.getAllUsers.filter((oneUser: User) =>
+                oneUser ? oneUser._id !== user._id : ''
+            );
+        }
+    }
+
     return (
         <SideMenuContainer>
             <SideMenu />
-            <div>Members Page</div>
+            <MemberPage>
+                <MemberTitle>Tous les autres membres</MemberTitle>
+                <MembersContainer>
+                    {otherUsers &&
+                        otherUsers.map((oneUser: UserMember) => (
+                            <MemberCard user={oneUser} />
+                        ))}
+                </MembersContainer>
+            </MemberPage>
         </SideMenuContainer>
     );
 };
