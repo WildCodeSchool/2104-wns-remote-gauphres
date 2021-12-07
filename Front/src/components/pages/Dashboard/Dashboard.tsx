405a0e15de79fbf83edf7a467781277b954270b3
Benoit Deligny <benoit.deligny@manomano.com>
2021-12-07 15:32:08 +0100

    MOOW-88: Adding random word's box

--- Front/src/components/pages/Dashboard/Dashboard.tsx
+++ Front/src/components/pages/Dashboard/Dashboard.tsx
@@ -1,16 +1,29 @@
-import React, { FC, useContext } from 'react';
-import { User, UserContext } from '../../../contexts/UserContext';
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable no-param-reassign */
+import React, { FC, useContext, useEffect, useState } from 'react';
+import { UserContext } from '../../../contexts/UserContext';
 import { SideMenuContainer } from '../../../style';
 import MyMood from '../../DailyWindow/MyMood/MyMood';
+import RandomWord from '../../DailyWindow/RandomWord/RandomWord';
 import SideMenu from '../../SideMenu/SideMenu';
 import { MainWrapper, RightWrapper, Wrapper } from './style';
 // import MyMatch from '../../DailyWindow/MyMatch';
 
 const Dashboard: FC = () => {
     const { user } = useContext(UserContext);
+    const [randomWord, setRandomWord] = useState();
 
-    // TODO : for test, mood id => to delete
-    // const id = '6096bf9fab2e797b569f4183';
+    const fetchData = async () => {
+        const response = await fetch(
+            'https://random-words-api.vercel.app/word'
+        );
+        const data = await response.json();
+        setRandomWord(data[0]);
+    };
+
+    useEffect(() => {
+        fetchData();
+    }, []);
 
     return (
         <SideMenuContainer>
@@ -21,6 +34,7 @@ const Dashboard: FC = () => {
                 </MainWrapper>
                 <RightWrapper>
                     <MyMood user={user} />
+                    <RandomWord randomWord={randomWord} />
                     {/* <MyMatch /> */}
                 </RightWrapper>
             </Wrapper>
