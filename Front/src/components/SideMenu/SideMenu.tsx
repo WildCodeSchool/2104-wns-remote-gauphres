50d0fe4ab89c20739867b0443786df5c8be6f58d
apsuma <delphine.belet@gmail.com>
2021-12-10 09:49:40 +0100

    add profilePage delete Articles and Events

--- Front/src/components/SideMenu/SideMenu.tsx
+++ Front/src/components/SideMenu/SideMenu.tsx
@@ -2,9 +2,9 @@
 import React, { FC, useContext } from 'react';
 import { gql, useMutation } from '@apollo/client';
 import { NavLink, useHistory } from 'react-router-dom';
-import { IoBeer, IoPersonAddSharp } from 'react-icons/io5';
+import { IoPersonAddSharp } from 'react-icons/io5';
 import { AiOutlineWechat } from 'react-icons/ai';
-import { RiArticleLine } from 'react-icons/ri';
+import { BsFillPersonFill } from 'react-icons/bs';
 import { BiCoffeeTogo } from 'react-icons/bi';
 import { MenuContainer, UsersConnectedContainer } from './style';
 import  UserConnected from './UserConnected';
@@ -42,10 +42,10 @@ const SideMenu: FC = () => {
         <div>
         <MenuContainer>  
             <NavLink to="/dashboard" activeStyle={{color: "purple"}}><BiCoffeeTogo /> Dashboard</NavLink>            
-            <NavLink to="/articles" activeStyle={{color: "purple"}}><RiArticleLine /> Articles</NavLink>
             <NavLink to="/random-chat" activeStyle={{color: "purple"}}><AiOutlineWechat /> Chat match</NavLink>
             <NavLink to="/members" activeStyle={{color: "purple"}}><IoPersonAddSharp /> Membres</NavLink>
-            <NavLink to="/events" activeStyle={{color: "purple"}}><IoBeer /> Evenements</NavLink>
+            <NavLink to="/profile" activeStyle={{color: "purple"}}><BsFillPersonFill /> Profile</NavLink>
+
         </MenuContainer>
         <Button color='secondary' onClick={handleDisconnect}>Déconnexion</Button>
         <UsersConnectedContainer>
