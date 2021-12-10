001242dc3509c355abdc2a573969c07a9082706d
BarbierAlexis <barbier-alexis@hotmail.fr>
2021-12-10 09:47:25 +0100

    Add scroll to chatView and fix css

--- Front/src/components/Chat/ChatView/ChatView.tsx
+++ Front/src/components/Chat/ChatView/ChatView.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect, useRef } from 'react';
 import { User } from '../../../types/authContextTypes';
 import { Container, BubbleMessage } from './style';
 
@@ -18,9 +18,27 @@ export const ChatView: FC<ChatViewProps> = ({
     messages,
     user,
 }: ChatViewProps) => {
+    const messageEl = useRef<HTMLDivElement>(null);
+    useEffect(() => {
+        if (messageEl.current) {
+            messageEl.current.addEventListener(
+                'DOMNodeInserted',
+                (event: any) => {
+                    const { currentTarget: target } = event;
+                    if (target) {
+                        target.scroll({
+                            top: target.scrollHeight,
+                            behavior: 'smooth',
+                        });
+                    }
+                }
+            );
+        }
+    }, []);
+
     return (
         <>
-            <Container>
+            <Container ref={messageEl}>
                 {messages.length > 0 &&
                     user !== null &&
                     messages.map((message) => {
--- Front/src/components/pages/RandomChatPage/style.js
+++ Front/src/components/pages/RandomChatPage/style.js
@@ -9,4 +9,5 @@ export const ChatPage = styled.div`
     display: flex;
     flex-direction: column;
     width: 60%;
+    align-items: center;
 `;
