ae647c573871f937dd9576d64a92308e4bf8037e
Benoit Deligny <benoit.deligny@manomano.com>
2021-12-14 16:23:54 +0100

    WIP deployement

--- deploy_branch_stg.sh
+++ deploy_branch_stg.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 echo "PORT=$PORT"
-git fetch origin && git reset --hard origin/master && git clean -f -d
+git fetch origin && git reset --hard origin/dev && git clean -f -d
 SUB_URL=staging.les-gauphres.wns.wilders.dev GATEWAY_PORT=$PORT docker-compose -f docker-compose-prod.yml up --build -d 
 docker system prune -f
