5b62e389e82de2b108f407a446391cf84c9892fd
Benoit Deligny <benoit.deligny@manomano.com>
2021-12-15 10:26:58 +0100

    FIW dploy

--- deploy_branch_stg.sh
+++ deploy_branch_stg.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 echo "PORT=$PORT"
+echo "SUB=$SUB"
 git fetch origin && git reset --hard origin/dev && git clean -f -d
-REACT_APP_SUB_URL=staging.les-gauphres.wns.wilders.dev GATEWAY_PORT=$PORT docker-compose -f docker-compose-prod.yml up --build -d 
+REACT_APP_SUB_URL=$SUB GATEWAY_PORT=$PORT docker-compose -f docker-compose-prod.yml up --build -d 
 docker system prune -f
