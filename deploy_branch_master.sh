66081be3f3ee6ace0692573fc3c16ac7b43d9da7
Benoit Deligny <benoit.deligny@manomano.com>
2021-12-15 10:17:09 +0100

    FIX deploy

--- deploy_branch_master.sh
+++ deploy_branch_master.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 echo "PORT=$PORT"
 git fetch origin && git reset --hard origin/master && git clean -f -d
-SUB_URL=les-gauphres.wns.wilders.dev GATEWAY_PORT=$PORT docker-compose -f docker-compose-prod.yml up --build -d 
+REACT_APP_SUB_URL=les-gauphres.wns.wilders.dev GATEWAY_PORT=$PORT docker-compose -f docker-compose-prod.yml up --build -d 
 docker system prune -f
